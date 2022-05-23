5da5c5b4d97b157a46d7f216cdc3cbebc727d2cf
lucas-evocorp <lucas@evocorp.com.br>
2022-05-23 17:49:23 -0300

    definindo usuario admininstrador e arrumando seeders

--- src/seeders/create-user-admin.seeder.ts
+++ src/seeders/create-user-admin.seeder.ts
@@ -5,21 +5,40 @@ import { PredefinedExercice } from 'src/training-histories/entities/predefined-e
 import { User } from 'src/users/entities/users.entity';
 import { UsersRepository } from 'src/users/repositories/users.repository';
 import { createQueryBuilder, Repository } from 'typeorm';
-import { userAdmin } from './predefineds-entities/predefined-admin-user';
 import { predefinedsExercices } from './predefineds-entities/predefined-exercices.seeder';
 import { predefinedMuscles } from './predefineds-entities/predefined-muscles';
+import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class insertUserAdminSeeder implements Seeder {
   constructor(private usersRepository: UsersRepository) {}
   async seed(): Promise<any> {
+    const hashedPassword = await bcrypt.hash(
+      process.env.ADMIN_PASSWORD,
+      parseInt(process.env.SALT_OR_ROUNDS),
+    );
     try {
-      return await this.usersRepository
-        .createQueryBuilder('users')
-        .insert()
-        .into(User)
-        .values(userAdmin)
-        .execute();
+      const userAdmin = await this.usersRepository.getUserByEmail(
+        process.env.ADMIN_EMAIL,
+      );
+
+      if (userAdmin && userAdmin.admin === true) {
+        console.error('admininstrador ja existe na aplicação');
+      } else {
+        return await this.usersRepository
+          .createQueryBuilder('users')
+          .insert()
+          .into(User)
+          .values({
+            admin: true,
+            email: process.env.ADMIN_EMAIL,
+            password: hashedPassword,
+            name: 'admininstrator',
+          })
+          .execute();
+      }
     } catch (error) {
       console.error('user seeder error');
     }
--- src/seeders/predefineds-entities/predefined-admin-user.ts
+++
@@ -1,6 +0,0 @@
-export const userAdmin = {
-  email: 'admin@gmail.com',
-  name: 'admininstrator',
-  password: 'admin12345',
-  admin: true,
-};
